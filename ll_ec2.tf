335187cd2aa4091327d6e2d8729698f36e2acf55
Nagaraja.S <nas@levi.com>
2023-06-16 13:34:35 +0530

    first commit

---
+++ ll_ec2.tf
@@ -0,0 +1,14 @@
+/*provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA5RYJ3XNAG4MPRG4A"
+  secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-057752b3f1d6c4d6c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}*/
